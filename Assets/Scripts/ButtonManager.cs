80ebcc86b64cdb86d211218ebc262ac0a1f53798
Asher <asher.haun@outlook.com>
2024-04-02 16:16:35 -0500

    butter was helping

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -3,30 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ButtonManager : MonoBehaviour
-{
+public class ButtonManager : MonoBehaviour {
     private List<InstantiateShips> instantiateShips = new List<InstantiateShips>();
     public Button[] buttons;
     private Button SelectedButton;
+
     // Start is called before the first frame update
-    void SelectButton(Button button)
-    {
-        if(SelectedButton != null)
-        SelectedButton = button;
-        {
-            Text selectedButtonText = SelectedButton.GetComponentInChildren<Text>();
-            selectedButtonText.fontStyle = FontStyle.Normal;
-        }
-        Text buttonText = button.GetComponentInChildren<Text>();
-        buttonText.fontStyle = FontStyle.Bold;
+    public void SelectButton(Button button) {
+        // if (SelectedButton != null)
+        //     SelectedButton = button;
+        // {
+        //     Text selectedButtonText = SelectedButton.GetComponentInChildren<Text>();
+        //     selectedButtonText.fontStyle = FontStyle.Normal;
+        // }
+        // Text buttonText = button.GetComponentInChildren<Text>();
+        // buttonText.fontStyle = FontStyle.Bold;
 
         Debug.Log(button.name);
     }
-    void Start()
-    {
-        foreach(Button button in buttons)
-        {
-            button.onClick.AddListener(() => SelectButton(button));
+
+    void Start() {
+        foreach (Button button in buttons) {
+            button.onClick.AddListener(() => {
+                Debug.Log("BUTTON WAS CLICKED! " + button.name);
+            });
             instantiateShips.Add(button.GetComponentInChildren<InstantiateShips>());
             instantiateShips[instantiateShips.Count - 1].enabled = false;
         }
@@ -34,8 +34,7 @@ public class ButtonManager : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
     }
 }
