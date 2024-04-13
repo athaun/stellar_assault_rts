e2ec02a9ab20670eaf0171667fe9099bfaf6e909
Asher <asher.haun@outlook.com>
2024-04-13 11:42:08 -0500

    remove ramiro (butter)'s button

--- Assets/Scripts/InstantiateShips.cs
+++ Assets/Scripts/InstantiateShips.cs
@@ -1,28 +1,22 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class InstantiateShips : MonoBehaviour
-{
+public class InstantiateShips : MonoBehaviour {
     public GameObject selectedShip;
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if(selectedShip != null && Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
-        {
+    void Update() {
+        if (selectedShip != null && Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject()) {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-            if(Physics.Raycast(ray, out hit))
-            {
-                if(hit.collider.tag == "BasePlane")
-                {
+            if (Physics.Raycast(ray, out hit)) {
+                if (hit.collider.tag == "BasePlane") {
                     Vector3 position = hit.point;
-                    position.y = 0.5f;
+                    position.y = 0.01f;
                     Instantiate(selectedShip, position, Quaternion.identity);
                 }
             }
