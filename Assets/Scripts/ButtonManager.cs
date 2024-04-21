142129d1da17ef1963d968fbb2d3c4e3c00c07b8
TheAmazingDoge <130805024+TheAmazingDoge@users.noreply.github.com>
2024-04-21 16:40:01 -0500

    Fixed buttons

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class ButtonManager : MonoBehaviour {
     private List<InstantiateShips> instantiateShips = new List<InstantiateShips>();
@@ -11,6 +12,10 @@ public class ButtonManager : MonoBehaviour {
 
     // Start is called before the first frame update
     public void SelectButton(Button button) {
+        if(SelectedButton == button) {
+            DeselectButton();
+            return;
+        }
         if (SelectedButton != null)
         {
             TextMeshProUGUI selectedButtonText = SelectedButton.GetComponentInChildren<TextMeshProUGUI>();
@@ -31,22 +36,53 @@ public class ButtonManager : MonoBehaviour {
         //Debug.Log(button.name);
     }
 
+    public void DeselectButton() {
+        if (SelectedButton != null)
+        {
+            TextMeshProUGUI selectedButtonText = SelectedButton.GetComponentInChildren<TextMeshProUGUI>();
+            selectedButtonText.fontStyle = FontStyles.Normal;
+            foreach (InstantiateShips instantiateShip in instantiateShips)
+            {
+                instantiateShip.enabled = false;
+            }
+            SelectedButton = null;
+        }
+    }
+
     void Start() {
         foreach (Button button in buttons) {
             instantiateShips.Add(button.GetComponentInChildren<InstantiateShips>());
             instantiateShips[instantiateShips.Count - 1].enabled = false;
-
             button.onClick.AddListener(() => {
                 //Debug.Log("BUTTON WAS CLICKED! " + button.name);
                 SelectButton(button);
             });
             
         }
-
+        InstantiateShips.OnShipInstantiated += DeselectButton;
     }
 
     // Update is called once per frame
+    void OnDestroy() {
+        InstantiateShips.OnShipInstantiated -= DeselectButton;
+    }
     void Update() {
-
+        if(SelectedButton != null && Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject()) {
+            PointerEventData pointerData = new PointerEventData(EventSystem.current);
+            pointerData.position = Input.mousePosition;
+            List<RaycastResult> results = new List<RaycastResult>();
+            EventSystem.current.RaycastAll(pointerData, results);
+            if(results.Count > 0)
+            {
+                foreach(RaycastResult result in results)
+                {
+                    if(result.gameObject.GetComponent<Button>() != null)
+                    {
+                        return;
+                    }
+                }
+            }
+            DeselectButton();
+        }
     }
 }
--- Assets/Scripts/InstantiateShips.cs
+++ Assets/Scripts/InstantiateShips.cs
@@ -1,15 +1,21 @@
+using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class InstantiateShips : MonoBehaviour {
+    public static event Action OnShipInstantiated;
     public GameObject selectedShip;
 
+    public ButtonManager buttonManager;
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
     void Update() {
-        if (selectedShip == null || EventSystem.current.IsPointerOverGameObject()) return;
-        if (Input.GetMouseButtonDown(0)) {
-            selectedShip = null;
-        }
-        if (Input.GetMouseButtonDown(1)) {
+        if (selectedShip != null && Input.GetMouseButtonDown(1) && !EventSystem.current.IsPointerOverGameObject()) {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit)) {
@@ -17,6 +23,7 @@ public class InstantiateShips : MonoBehaviour {
                     Vector3 position = hit.point;
                     position.y = 0.01f;
                     Instantiate(selectedShip, position, Quaternion.identity);
+                    OnShipInstantiated?.Invoke();
                 }
             }
         }
