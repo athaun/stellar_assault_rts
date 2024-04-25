0bb74e1461cc8147b4826ba252822cd40a8a407a
Asher <asher.haun@outlook.com>
2024-04-25 13:34:45 -0500

    spawner coroutine

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -10,9 +10,23 @@ public class EnemySpawner : MonoBehaviour {
     public int numberOfShips = 10;
     public float spawnRadius = 10f;
 
+
     void Start() {
+        SpawnShips();
+        StartCoroutine(SpawnShipsRoutine());
+    }
+
+    IEnumerator SpawnShipsRoutine() {
+        while (true) {
+            SpawnShips();
+            yield return new WaitForSeconds(20);
+        }
+    }
+
+    void SpawnShips() {
+        Debug.Log("Spawning ships");
         for (int i = 0; i < numberOfShips; i++) {
-            Vector3 spawnPosition = Random.insideUnitSphere * spawnRadius;
+            Vector3 spawnPosition = Random.onUnitSphere * spawnRadius;
             spawnPosition.y = 0;
 
             Ship ship = Instantiate(shipPrefabs[Random.Range(0, shipPrefabs.Length)], spawnPosition, Quaternion.identity);
@@ -20,8 +34,7 @@ public class EnemySpawner : MonoBehaviour {
             ship.gameObject.GetComponent<Ship>().enabled = false;
             ship.gameObject.GetComponent<EnemyShip>().enabled = true;
             ship.gameObject.GetComponent<EnemyShip>().SpaceStation = spaceStation;            
-
-            // Debug.Log("Created enemy at " + spawnPosition + " of type " + ship);
         }
     }
+
 }
